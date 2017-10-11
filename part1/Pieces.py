7e04e089b4af5477ffe1aab0ebc60173a2f628be
anduer <anduer@iu.edu>
2017-10-11 16:59:13 -0700

    All pieces complete.
    Added pawns and queens. I really want to make Queens, Rooks, and Bishops less ugly. Looks like I -funroll'd the entire function. >.<

--- part1/Pieces.py
+++ part1/Pieces.py
@@ -13,11 +13,16 @@ class King(Piece):
         else: return u'♚'
         
     def moves(self, board):
-        moves = [(self.row+1, self.col), (self.row-1, self.col), (self.row, self.col+1), (self.row, self.col-1)]
+        moves = []
+        for i in [1, -1]:
+            moves.append((self.row+i, self.col))
+            moves.append((self.row, self.col+i))
+
         possible = []
         
         for move in moves:
-            if valid_move(board, self, move[0], move[1]): possible.append(((self.row, self.col), move))
+            valid, _ = valid_move(board, self, move[0], move[1])
+            if valid: possible.append(((self.row, self.col), move))
         return possible
 
 class Knight(Piece):
@@ -35,7 +40,8 @@ class Knight(Piece):
         possible = []
         
         for move in moves:
-            if valid_move(board, self, move[0], move[1]): possible.append(((self.row, self.col), move))
+            valid, _ = valid_move(board, self, move[0], move[1])
+            if valid: possible.append(((self.row, self.col), move))
         return possible
    
 class Rook(Piece):
@@ -48,31 +54,27 @@ class Rook(Piece):
         
         #Up
         for u in range(1, 8 - self.row):
-            if valid_move(board, self, self.row + u, self.col):
-                possible.append(((self.row, self.col), (self.row + u, self.col)))
-            else: break
-            if board[self.row + u][self.col] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row + u, self.col)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col)))
+            if (not valid) or (not cont): break
 
         #Down
         for u in range(1, self.row):
-            if valid_move(board, self, self.row - u, self.col):
-                possible.append(((self.row, self.col), (self.row - u, self.col)))
-            else: break
-            if board[self.row - u][self.col] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row - u, self.col)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col)))
+            if (not valid) or (not cont): break
         
         #Right
         for u in range(1, 8 - self.col):
-            if valid_move(board, self, self.row, self.col + u):
-                possible.append(((self.row, self.col), (self.row, self.col + u)))
-            else: break
-            if board[self.row][self.col + u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row, self.col + u)))
+            if (not valid) or (not cont): break
 
         #Left
         for u in range(1, self.col):
-            if valid_move(board, self, self.row, self.col - u):
-                possible.append(((self.row, self.col), (self.row, self.col - u)))
-            else: break
-            if board[self.row][self.col - u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row, self.col - u)))
+            if (not valid) or (not cont): break
         
         return possible   
  
@@ -86,53 +88,121 @@ class Bishop(Piece):
         
         #Up/Right
         for u in range(1, min(8 - self.row, 8 - self.col)):
-            if valid_move(board, self, self.row + u, self.col + u):
-                possible.append(((self.row, self.col), (self.row + u, self.col + u)))
-            else: break
-            if board[self.row + u][self.col + u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row + u, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col + u)))
+            if (not valid) or (not cont): break
 
         #Up/Left
         for u in range(1, min(8 - self.row, self.col + 1)):
-            if valid_move(board, self, self.row + u, self.col - u):
-                possible.append(((self.row, self.col), (self.row + u, self.col - u)))
-            else: break
-            if board[self.row + u][self.col - u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row + u, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col - u)))
+            if (not valid) or (not cont): break
         
         #Down/Right
         for u in range(1, min(self.row + 1, 8 - self.col)):
-            if valid_move(board, self, self.row - u, self.col + u):
-                possible.append(((self.row, self.col), (self.row - u, self.col + u)))
-            else: break
-            if board[self.row - u][self.col + u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row - u, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col + u)))
+            if (not valid) or (not cont): break
 
         #Down/Left
         for u in range(1, min(self.row + 1, self.col + 1)):
-            if valid_move(board, self, self.row - u, self.col - u):
-                possible.append(((self.row, self.col), (self.row - u, self.col - u)))
-            else: break
-            if board[self.row - u][self.col - u] != '': break #Stop if we hit a piece.
+            valid, cont = valid_move(board, self, self.row - u, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col - u)))
+            if (not valid) or (not cont): break
 
         return possible          
-        
         
 class Queen(Piece):
     def __repr__(self):
         if self.color == "w": return u'♕'
         else: return u'♛'        
+        
+    def moves(self, board):
+        possible = []
+        
+        #Up/Right
+        for u in range(1, min(8 - self.row, 8 - self.col)):
+            valid, cont = valid_move(board, self, self.row + u, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col + u)))
+            if (not valid) or (not cont): break
+
+        #Up/Left
+        for u in range(1, min(8 - self.row, self.col + 1)):
+            valid, cont = valid_move(board, self, self.row + u, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col - u)))
+            if (not valid) or (not cont): break
+        
+        #Down/Right
+        for u in range(1, min(self.row + 1, 8 - self.col)):
+            valid, cont = valid_move(board, self, self.row - u, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col + u)))
+            if (not valid) or (not cont): break
+
+        #Down/Left
+        for u in range(1, min(self.row + 1, self.col + 1)):
+            valid, cont = valid_move(board, self, self.row - u, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col - u)))
+            if (not valid) or (not cont): break
+
+        #Up
+        for u in range(1, 8 - self.row):
+            valid, cont = valid_move(board, self, self.row + u, self.col)
+            if valid: possible.append(((self.row, self.col), (self.row + u, self.col)))
+            if (not valid) or (not cont): break
+
+        #Down
+        for u in range(1, self.row):
+            valid, cont = valid_move(board, self, self.row - u, self.col)
+            if valid: possible.append(((self.row, self.col), (self.row - u, self.col)))
+            if (not valid) or (not cont): break
+        
+        #Right
+        for u in range(1, 8 - self.col):
+            valid, cont = valid_move(board, self, self.row, self.col + u)
+            if valid: possible.append(((self.row, self.col), (self.row, self.col + u)))
+            if (not valid) or (not cont): break
+
+        #Left
+        for u in range(1, self.col):
+            valid, cont = valid_move(board, self, self.row, self.col - u)
+            if valid: possible.append(((self.row, self.col), (self.row, self.col - u)))
+            if (not valid) or (not cont): break        
+        
+        return possible
 
 class Pawn(Piece):
     def __repr__(self):
         if self.color == "w": return u'♙'
         else: return u'♟'
-   
-     
+        
+    def moves(self, board):
+        possible = []
+        
+        if self.color == "w": step = 1
+        else: step = -1
+        
+        #Forward movement
+        valid, cont = valid_move(board, self, self.row + step, self.col)
+        if valid: possible.append(((self.row, self.col), (self.row + step, self.col)))
+        if valid and cont and ((self.row == 1 and self.color == "w") or (self.row == 6 and self.color == "b")): 
+            valid, cont = valid_move(board, self, self.row + step * 2, self.col)
+            if valid: possible.append(((self.row, self.col), (self.row + step * 2, self.col)))
+            
+        #Capture
+        valid, cont = valid_move(board, self, self.row + step, self.col + 1)
+        if valid and not cont: possible.append(((self.row, self.col), (self.row + step, self.col + 1)))        
+            
+        valid, cont = valid_move(board, self, self.row + step, self.col - 1)
+        if valid and not cont: possible.append(((self.row, self.col), (self.row + step, self.col - 1)))        
+
+        return possible
         
 def valid_move(board, piece, row, col):
-        if row < 0 or row > 7 or col < 0 or col > 7: #Out of Bounds
-            return False
-        elif board[row][col] == "": #Move into empty space.
-            return True
-        elif board[row][col].color != piece.color: #Capture
-            return True
-        else:
-            return False
+    if row < 0 or row > 7 or col < 0 or col > 7: #Out of Bounds
+        return False, False
+    elif board[row][col] == "": #Move into empty space.
+        return True, True
+    elif board[row][col].color != piece.color: #Capture
+        return True, False
+    else:
+        return False, False
