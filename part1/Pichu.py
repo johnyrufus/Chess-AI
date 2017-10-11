2055649a512c985e8a22244eca0be607980c5dd6
anduer <anduer@iu.edu>
2017-10-10 22:47:59 -0700

    Remove test cases
    Moved out into its own class - will make it easier to remove once out of testing.

--- part1/Pichu.py
+++ part1/Pichu.py
@@ -12,66 +12,8 @@ N/n = Nighthawk = Knight
 Rules: no castling, no check, no en passant.
 '''
 import Board
+import UnitTests
 from datetime import datetime
-
-def print_state(state):
-    print(*state, sep="\n")
-
-def unit_tests():
-    #Checking king movement when alone
-    test = '...........................K....................................'
-    test_case = Board.PlayBoard(test)
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 3)), ((3, 3), (2, 3)), ((3, 3), (3, 4)), ((3, 3), (3, 2))])
-
-    #Checking king movement when surrounded
-    test = '...................P......PKP......P............................'
-    test_case = Board.PlayBoard(test)
-    assert(test_case.state[3][3].moves(test_case.state) == [])    
-
-    #Checking king movement when capture
-    test = '...................p......pKp......p............................'
-    test_case = Board.PlayBoard(test)
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 3)), ((3, 3), (2, 3)), ((3, 3), (3, 4)), ((3, 3), (3, 2))])
-
-    #Checking knight movement when alone
-    test = '...........................N....................................'
-    test_case = Board.PlayBoard(test)    
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (1, 2)), ((3, 3), (2, 1)), ((3, 3), (1, 4)), ((3, 3), (4, 1)), \
-                                                            ((3, 3), (5, 2)), ((3, 3), (2, 5)), ((3, 3), (5, 4)), ((3, 3), (4, 5))])
-
-    #Checking knight when surrounded.
-    test = '..........P.P....P...P.....N.....P...P....P.P...................'
-    test_case = Board.PlayBoard(test)    
-    assert(test_case.state[3][3].moves(test_case.state) == [])
-
-    #Checking knight capture.
-    test = '..........P.P....P...P.....n.....P...P....P.P...................'
-    test_case = Board.PlayBoard(test)    
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (1, 2)), ((3, 3), (2, 1)), ((3, 3), (1, 4)), ((3, 3), (4, 1)), \
-                                                            ((3, 3), (5, 2)), ((3, 3), (2, 5)), ((3, 3), (5, 4)), ((3, 3), (4, 5))])
-
-    #Checking rook capture.
-    test = '...................p......pRp..............p....................'
-    test_case = Board.PlayBoard(test)    
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 3)), ((3, 3), (5, 3)), ((3, 3), (2, 3)), \
-                                                           ((3, 3), (3, 4)), ((3, 3), (3, 2))])
-
-    #Checking rook surrounded.
-    test = '...................P......PRP......P............................'
-    test_case = Board.PlayBoard(test)    
-    assert(test_case.state[3][3].moves(test_case.state) == [])
-
-    #Checking Bishop capture.
-    test = '..................p.p......B......p.p...........................'
-    test_case = Board.PlayBoard(test)
-    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 4)), ((3, 3), (4, 2)), ((3, 3), (2, 4)), \
-                                                           ((3, 3), (2, 2))])
-
-    #Checking Bishop surrounded.
-    test = '..................P.P......B......P.P...........................'
-    test_case = Board.PlayBoard(test)
-    assert(test_case.state[3][3].moves(test_case.state) == [])
-
     
 if __name__ == "__main__":
     start_time = datetime.now()
@@ -80,6 +22,6 @@ if __name__ == "__main__":
     state = 'RNBQKBNRPPPPPPPP................................pppppppprnbqkbnr'
     time_limit = 10
     
-    root = Board.PlayBoard(state)
+    root = Board.Parse(state)
     
-    unit_tests()
+    UnitTests.run_tests()
