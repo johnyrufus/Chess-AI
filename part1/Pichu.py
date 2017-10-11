f14cd43df7774a3bd859de4718ceb853b40c5581
anduer <anduer@iu.edu>
2017-10-10 22:36:04 -0700

    Rooks and Bishops
    Added test cases for rooks and bishops.

--- part1/Pichu.py
+++ part1/Pichu.py
@@ -39,7 +39,39 @@ def unit_tests():
     assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (1, 2)), ((3, 3), (2, 1)), ((3, 3), (1, 4)), ((3, 3), (4, 1)), \
                                                             ((3, 3), (5, 2)), ((3, 3), (2, 5)), ((3, 3), (5, 4)), ((3, 3), (4, 5))])
 
-    
+    #Checking knight when surrounded.
+    test = '..........P.P....P...P.....N.....P...P....P.P...................'
+    test_case = Board.PlayBoard(test)    
+    assert(test_case.state[3][3].moves(test_case.state) == [])
+
+    #Checking knight capture.
+    test = '..........P.P....P...P.....n.....P...P....P.P...................'
+    test_case = Board.PlayBoard(test)    
+    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (1, 2)), ((3, 3), (2, 1)), ((3, 3), (1, 4)), ((3, 3), (4, 1)), \
+                                                            ((3, 3), (5, 2)), ((3, 3), (2, 5)), ((3, 3), (5, 4)), ((3, 3), (4, 5))])
+
+    #Checking rook capture.
+    test = '...................p......pRp..............p....................'
+    test_case = Board.PlayBoard(test)    
+    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 3)), ((3, 3), (5, 3)), ((3, 3), (2, 3)), \
+                                                           ((3, 3), (3, 4)), ((3, 3), (3, 2))])
+
+    #Checking rook surrounded.
+    test = '...................P......PRP......P............................'
+    test_case = Board.PlayBoard(test)    
+    assert(test_case.state[3][3].moves(test_case.state) == [])
+
+    #Checking Bishop capture.
+    test = '..................p.p......B......p.p...........................'
+    test_case = Board.PlayBoard(test)
+    assert(test_case.state[3][3].moves(test_case.state) == [((3, 3), (4, 4)), ((3, 3), (4, 2)), ((3, 3), (2, 4)), \
+                                                           ((3, 3), (2, 2))])
+
+    #Checking Bishop surrounded.
+    test = '..................P.P......B......P.P...........................'
+    test_case = Board.PlayBoard(test)
+    assert(test_case.state[3][3].moves(test_case.state) == [])
+
     
 if __name__ == "__main__":
     start_time = datetime.now()
@@ -49,6 +81,5 @@ if __name__ == "__main__":
     time_limit = 10
     
     root = Board.PlayBoard(state)
-    print(*root.state, sep='\n')
     
     unit_tests()
