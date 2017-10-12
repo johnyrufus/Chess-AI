9e7fefd15d4d4b3b7b685cd62ffcd4a9818bf28d
anduer <anduer@iu.edu>
2017-10-11 21:31:03 -0700

    Update Pieces.py

--- part1/Pieces.py
+++ part1/Pieces.py
@@ -6,42 +6,43 @@ class Piece():
         self.color = color
         self.row = row
         self.col = col
-
+        self.pos = (row, col)
+        
+    def offset(self, r, c):
+        return (self.row + r, self.col + c)
+    
 class King(Piece):
     def __repr__(self):
         if self.color == "w": return u'♔'
         else: return u'♚'
         
     def moves(self, board):
-        moves = []
-        for i in [1, -1]:
-            moves.append((self.row+i, self.col))
-            moves.append((self.row, self.col+i))
-
         possible = []
         
-        for move in moves:
-            valid, _ = valid_move(board, self, move[0], move[1])
-            if valid: possible.append(((self.row, self.col), move))
+        for i in [1, -1]:
+            valid, _ = ValidMove(board, self, self.row + i, self.col)
+            if valid: possible.append((self.pos, self.offset(i, 0)))
+        
+            valid, _ = ValidMove(board, self, self.row, self.col + i)
+            if valid: possible.append((self.pos, self.offset(0, i)))
+
         return possible
 
 class Knight(Piece):
+    move_r = [-2, -1, -2, 1, 2, -1, 2, 1]
+    move_c = [-1, -2, 1, -2, -1, 2, 1, 2]
+    
     def __repr__(self):
         if self.color == "w": return u'♘'
         else: return u'♞'   
 
-    def moves(self, board):
-        moves = []
-        for v1 in [-2, 2]:
-            for v2 in [-1, 1]:
-                moves.append((self.row+v1, self.col+v2))
-                moves.append((self.row+v2, self.col+v1))
-        
+    def moves(self, board):        
         possible = []
         
-        for move in moves:
-            valid, _ = valid_move(board, self, move[0], move[1])
-            if valid: possible.append(((self.row, self.col), move))
+        for r, c in zip(self.move_r, self.move_c):
+            valid, _ = ValidMove(board, self, self.row + r, self.col + c)
+            if valid: possible.append((self.pos, self.offset(r, c)))
+
         return possible
    
 class Rook(Piece):
@@ -52,29 +53,9 @@ class Rook(Piece):
     def moves(self, board):
         possible = []
         
-        #Up
-        for u in range(1, 8 - self.row):
-            valid, cont = valid_move(board, self, self.row + u, self.col)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col)))
-            if (not valid) or (not cont): break
-
-        #Down
-        for u in range(1, self.row):
-            valid, cont = valid_move(board, self, self.row - u, self.col)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col)))
-            if (not valid) or (not cont): break
-        
-        #Right
-        for u in range(1, 8 - self.col):
-            valid, cont = valid_move(board, self, self.row, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Left
-        for u in range(1, self.col):
-            valid, cont = valid_move(board, self, self.row, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row, self.col - u)))
-            if (not valid) or (not cont): break
+        for i in [-1, 1]:
+            possible += GoInDirection(board, self, i, 0) #Up/Down
+            possible += GoInDirection(board, self, 0, i) #Left/Right
         
         return possible   
  
@@ -86,30 +67,10 @@ class Bishop(Piece):
     def moves(self, board):
         possible = []
         
-        #Up/Right
-        for u in range(1, min(8 - self.row, 8 - self.col)):
-            valid, cont = valid_move(board, self, self.row + u, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Up/Left
-        for u in range(1, min(8 - self.row, self.col + 1)):
-            valid, cont = valid_move(board, self, self.row + u, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col - u)))
-            if (not valid) or (not cont): break
-        
-        #Down/Right
-        for u in range(1, min(self.row + 1, 8 - self.col)):
-            valid, cont = valid_move(board, self, self.row - u, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Down/Left
-        for u in range(1, min(self.row + 1, self.col + 1)):
-            valid, cont = valid_move(board, self, self.row - u, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col - u)))
-            if (not valid) or (not cont): break
-
+        for i in [-1, 1]:
+            possible += GoInDirection(board, self, i, i) #DownLeft/UpRight
+            possible += GoInDirection(board, self, i, -i) #DownRight/UpLeft
+            
         return possible          
         
 class Queen(Piece):
@@ -120,53 +81,11 @@ class Queen(Piece):
     def moves(self, board):
         possible = []
         
-        #Up/Right
-        for u in range(1, min(8 - self.row, 8 - self.col)):
-            valid, cont = valid_move(board, self, self.row + u, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Up/Left
-        for u in range(1, min(8 - self.row, self.col + 1)):
-            valid, cont = valid_move(board, self, self.row + u, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col - u)))
-            if (not valid) or (not cont): break
-        
-        #Down/Right
-        for u in range(1, min(self.row + 1, 8 - self.col)):
-            valid, cont = valid_move(board, self, self.row - u, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Down/Left
-        for u in range(1, min(self.row + 1, self.col + 1)):
-            valid, cont = valid_move(board, self, self.row - u, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col - u)))
-            if (not valid) or (not cont): break
-
-        #Up
-        for u in range(1, 8 - self.row):
-            valid, cont = valid_move(board, self, self.row + u, self.col)
-            if valid: possible.append(((self.row, self.col), (self.row + u, self.col)))
-            if (not valid) or (not cont): break
-
-        #Down
-        for u in range(1, self.row):
-            valid, cont = valid_move(board, self, self.row - u, self.col)
-            if valid: possible.append(((self.row, self.col), (self.row - u, self.col)))
-            if (not valid) or (not cont): break
-        
-        #Right
-        for u in range(1, 8 - self.col):
-            valid, cont = valid_move(board, self, self.row, self.col + u)
-            if valid: possible.append(((self.row, self.col), (self.row, self.col + u)))
-            if (not valid) or (not cont): break
-
-        #Left
-        for u in range(1, self.col):
-            valid, cont = valid_move(board, self, self.row, self.col - u)
-            if valid: possible.append(((self.row, self.col), (self.row, self.col - u)))
-            if (not valid) or (not cont): break        
+        for i in [-1, 1]:
+            possible += GoInDirection(board, self, i, 0) #Up/Down
+            possible += GoInDirection(board, self, 0, i) #Left/Right
+            possible += GoInDirection(board, self, i, i) #DownLeft/UpRight
+            possible += GoInDirection(board, self, i, -i) #DownRight/UpLeft
         
         return possible
 
@@ -182,22 +101,22 @@ class Pawn(Piece):
         else: step = -1
         
         #Forward movement
-        valid, cont = valid_move(board, self, self.row + step, self.col)
-        if valid: possible.append(((self.row, self.col), (self.row + step, self.col)))
+        valid, cont = ValidMove(board, self, self.row + step, self.col)
+        if valid: possible.append((self.pos, (self.row + step, self.col)))
         if valid and cont and ((self.row == 1 and self.color == "w") or (self.row == 6 and self.color == "b")): 
-            valid, cont = valid_move(board, self, self.row + step * 2, self.col)
-            if valid: possible.append(((self.row, self.col), (self.row + step * 2, self.col)))
+            valid, cont = ValidMove(board, self, self.row + step * 2, self.col)
+            if valid: possible.append((self.pos, (self.row + step * 2, self.col)))
             
         #Capture
-        valid, cont = valid_move(board, self, self.row + step, self.col + 1)
-        if valid and not cont: possible.append(((self.row, self.col), (self.row + step, self.col + 1)))        
+        valid, cont = ValidMove(board, self, self.row + step, self.col + 1)
+        if valid and not cont: possible.append((self.pos, (self.row + step, self.col + 1)))        
             
-        valid, cont = valid_move(board, self, self.row + step, self.col - 1)
-        if valid and not cont: possible.append(((self.row, self.col), (self.row + step, self.col - 1)))        
+        valid, cont = ValidMove(board, self, self.row + step, self.col - 1)
+        if valid and not cont: possible.append((self.pos, (self.row + step, self.col - 1)))        
 
         return possible
         
-def valid_move(board, piece, row, col):
+def ValidMove(board, piece, row, col):
     if row < 0 or row > 7 or col < 0 or col > 7: #Out of Bounds
         return False, False
     elif board[row][col] == "": #Move into empty space.
@@ -206,3 +125,21 @@ def valid_move(board, piece, row, col):
         return True, False
     else:
         return False, False
+    
+def GoInDirection(board, piece, row, col):
+    moves = []
+    
+    r = row
+    c = col
+    
+    valid, cont = ValidMove(board, piece, piece.row + r, piece.col + c)
+    while valid:
+        moves.append((piece.pos, piece.offset(r, c)))
+        
+        if not cont: return moves
+        
+        r += row
+        c += col
+        valid, cont = ValidMove(board, piece, piece.row + r, piece.col + c)
+    
+    return moves
