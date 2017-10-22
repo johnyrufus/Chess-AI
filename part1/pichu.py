ce4a070ec2b46f59aac57acbdbf0a05c4b591737
anduer <anduer@iu.edu>
2017-10-21 18:22:15 -0700

    Documentation
    Wrote documentation and cleaned up some of the testing we had been doing.

--- part1/pichu.py
+++ part1/pichu.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-'''
-P/p = Parakeet = Pawn
-R/r = Robin = Rook
-B/b = Bluejay = Bishop
-Q/q = Quetzal = Queen
-K/k = Kingfisher = King
-N/n = Nighthawk = Knight
-Rules: no castling, no check, no en passant.
-'''
 import sys
 import Board
 import Algorithms
 import time
 from multiprocessing import Process
+
+'''
+The pichu program is what reads in the user's input and then kicks off our
+minimax search. The basic_parallel_minimax function is what we use to search
+to various depths. The worker class is a simple process we can kick off to 
+search to a depth given by i. We have defaulted the depth to 4 is that is 
+what testing on burrow could be reached in a reasonable amount of time.
+
+Due to the time constraint, however, we do not have certainty that we can
+complete a depth of search 4. To safeguard against that, we do a quick search
+at depths 1 and 2 to get an initial move to the user. Then we search 3 and 4
+in parallel to maximize the likelihood that we can complete a full search.
+
+The program then finishes by waiting for each process to finish by calling
+join().
+
+What we tried and didn't work:
+- The biggest obstacle we ran into was how to search and respect the timer.
+Our initial thought was to simply remove the depth value so that the function
+would search until it ran out of time. However, and retrospecively obviously,
+all this accomplished was causing a runaway search on one branch until the
+timer began to runout and then heuristic estimation of every other branch. This
+is why we ended up with doing iterative deepening search as a safeguard.
+
+- Another attempt to solve the deepening issue was to search to a depth and 
+then have each process wait until a signal was given to start searching at a
+deeper depth. This process, however, proved too cumbersome to achieve in a
+reasonable amount of time.
+'''
 
 def basic_parallel_minimax(player, board, timer, start):
 
@@ -22,9 +42,8 @@ def basic_parallel_minimax(player, board, timer, start):
         game = Algorithms.Minimax(board, player, i, timer, start)
         move = game.MiniMaxSearch()
         new_board = board.move(move[0], move[1])
-        #print(new_board)
 
-    max_depth = 3
+    max_depth = 4
     procs = list()
 
     # For depths upto 2,  handle it serially, rest handle them parallely.
@@ -42,18 +61,4 @@ def basic_parallel_minimax(player, board, timer, start):
 if __name__ == "__main__":
     start = time.clock()
  
-    player = 'w'
-    state =   'RNBQKBNR' \
-            + 'PPPP.PPP' \
-            + '....P...' \
-            + '........' \
-            + '........' \
-            + '.......p' \
-            + 'ppppppp.' \
-            + 'rnbqkbnr'
-    time_limit = 10
-
-    root = Board.Parse(state)
-    #print(root.PrintOut())
     basic_parallel_minimax(sys.argv[1], Board.Parse(sys.argv[2]), int(sys.argv[3]))
-    #print('Time taken for Pichu overall execution -  {} '.format(time.clock() - start))  # TODO: Comment this before final
